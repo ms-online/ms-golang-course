efdd9c0a24c2583f3f88676c3b5a0cf0306f257c
ms-online <27732357@qq.com>
2021-09-14 16:16:05 +0800

    lesson-3

--- main.go
+++ main.go
@@ -2,50 +2,30 @@ package main
 
 import "fmt"
 
-var someName = "someOne"
-
 func main() { 
+	age := 25
+	name := "summer"
 
-	// 字符串 
-	// var nameOne string = "summer"
-	// var nameTwo = "lucy"
-	// var nameThree string 
+	// Print
+	fmt.Print("hello, ")
+	fmt.Print("world! \n")
+	fmt.Print("Hi! \n")
+  
+	// Println
+	fmt.Println("你好！")
+	fmt.Println("我是Summer.")
+	fmt.Println("我的名字叫",name, ",今年",age,"周岁。")
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
+	// Printf : 格式化输出函数
+   fmt.Printf("我的名字叫%v,今年%v周岁。\n", name, age)
+   fmt.Printf("我的名字叫%q,今年%q周岁。\n", name, age)
+   fmt.Printf("age变量的类型为%T \n",age)
+	 fmt.Printf("你的得分为%0.1f! \n",95.56)
 
-	// nameOne = "Summer"
-	// nameThree = "Henry"
+	 //Sprintf: 格式化并返回一个字符串（不带输出）
+	var str =  fmt.Sprintf("我的名字叫%v,今年%v周岁。\n", name, age)
+	fmt.Print(str)
 
-	// fmt.Println(nameOne, nameThree)
-
-	// nameFour := "John"
-
-	// fmt.Println(nameFour)
-
-
-	
-	// 整数
-	var ageOne int = 18
-	var ageTwo = 20
-	ageThree := 22
-
-	fmt.Println(ageOne, ageTwo,ageThree)
-	
-  // https://pkg.go.dev/builtin
-	// https://golang.org/ref/spec#Numeric_types
-  //bits位
-	var numOne int8 = 50
-	var numTwo int16 = 300
-	var numThree uint8 = 25
-
-	//浮点
-	var scoreOne float32 = 87.32
-	var scoreTwo float64 = 3243234324.2
-	scoreThree := 0.5
-
-	fmt.Println(scoreOne, scoreTwo,scoreThree)
-
-
-	fmt.Println(numOne,numTwo,numThree)
+	// 格式化符号：https://pkg.go.dev/fmt@go1.17.1
 
 }
