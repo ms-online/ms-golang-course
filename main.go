b7104d2f46befd72f9370d9a8bb7df95e1c08e4e
ms-online <27732357@qq.com>
2021-09-15 15:57:51 +0800

    lesson-7

--- main.go
+++ main.go
@@ -3,31 +3,38 @@ package main
 import "fmt"
 
 func main() { 
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("x的值为：", x)
-	// 	x++
+	// age := 55
+ // 布尔值
+	// fmt.Println( age < 40)
+	// fmt.Println( age <= 40)
+	// fmt.Println( age > 40)
+	// fmt.Println( age >= 40)
+	// fmt.Println( age == 35)
+	// fmt.Println( age != 35)
+
+
+	// 条件语句
+	// if age < 40{
+	// 	fmt.Println("年龄在40周岁以下")
+	// } else if age < 50 {
+	// 	fmt.Println("年龄在50周岁以下")
+	// } else {
+	// 	fmt.Println("年龄在55周岁及其以上")
 	// }
+	
+ 	names := []string{"summer","lucy","henry","tom","lily"}
 
-	// for i :=0; i < 5; i++ {
-	// 	fmt.Println("i的值为：", i)
-	// }
+	 for index, value := range names {
+    if index == 1 {
+			fmt.Println("当前下标为：", index)
+			continue
+		}
 
-	names := []string{"summer","lucy","henry","tom"}
+		if index > 2 {
+			fmt.Println("下标值满足大于2的条件，此时索引为：", index)
+			break
+		}
 
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
-  
-	// for index, value := range names {
-	// 	fmt.Printf("下标 %v 对应的值为 %v \n", index, value)
-	// }
-
-		for _, value := range names {
-				fmt.Printf("遍历的值为 %v \n", value)
-				value = "新名称"
-			}
-  // 不改变原始值
-		fmt.Println(names)
-
+		 fmt.Printf("下标 %v 的值为 %v \n", index, value)
+	 }
 }
