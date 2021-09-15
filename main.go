833be18d8ba13e718f587832b72e8a1346df25bc
ms-online <27732357@qq.com>
2021-09-15 17:43:32 +0800

    lesson-9

--- main.go
+++ main.go
@@ -2,43 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
+func getFirstWords(n string) (string, string) {
+    s := strings.ToUpper(n)
+		names := strings.Split(s," ")
 
-func sayGreeting(n string) {
-	fmt.Printf("早上好，%v! \n", n)
+		var firstWords []string
+		for _, v := range names {
+			firstWords = append(firstWords, v[:1])
+		}
+
+		if len(firstWords) > 1 {
+			return firstWords[0], firstWords[1]
+		}
+
+		return firstWords[0], "_"
+
 }
-
-func sayBye(n string) {
-	fmt.Printf("再见，%v \n", n)
-}
-
-func cycleNames(n []string, f func(string)){
-	for _, v := range n {
-		f(v)
-	}
-}
-
-func circleArea(r float64) float64 {
-	return	math.Pi * r * r
-}
-
 
 func main() { 
-	// len() append()
+ fn1, sn1 :=	getFirstWords("summer peng")
+ fmt.Println(fn1,sn1)
 
-	sayGreeting("summer")
-	sayBye("lucy")
-	sayBye("tom")
+ fn2, sn2 :=	getFirstWords("lucy deng")
+ fmt.Println(fn2,sn2)
 
-	cycleNames([]string{"summer","lucy","henry"}, sayGreeting)
-	cycleNames([]string{"summer","lucy","henry"}, sayBye)
-
-	 a1 := circleArea(10)
-	 a2 := circleArea(15.5)
-
-	 fmt.Println(a1, a2)
-	 fmt.Printf("a1的面积为：%0.3f, a2的面积为：%0.3f", a1,a2)
+ fn3, sn3 :=	getFirstWords("henry")
+ fmt.Println(fn3,sn3)
 }
 
